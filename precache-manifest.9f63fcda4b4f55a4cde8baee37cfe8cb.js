583990a7a1b1cdcd9ab6d3f40306a7f57c952e9f
Tushar Khatri <tusharkhatri20@gmail.com>
2020-09-15 13:57:22 +0530

    Updates

---
+++ precache-manifest.9f63fcda4b4f55a4cde8baee37cfe8cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cf3922f9169057471efbf31e979823a",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "394922d06e18f0a9ee9d",
+    "url": "/movie-search-app/static/js/2.1f882c7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-search-app/static/js/2.1f882c7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f937ceb3b25837d4cedd",
+    "url": "/movie-search-app/static/js/main.be28dff6.chunk.js"
+  },
+  {
+    "revision": "93854698e18f7c10401e",
+    "url": "/movie-search-app/static/js/runtime-main.fb6de319.js"
+  }
+]);
